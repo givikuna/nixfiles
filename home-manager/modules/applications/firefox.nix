b5916194e4b2ecd9c848c8a6fd612eefdef06001
Givikuna <thommaster10@gmail.com>
2026-05-22 19:47:54 -0400

    moving userContent to stylus instead because it keeps breaking

--- home-manager/modules/applications/firefox.nix
+++ home-manager/modules/applications/firefox.nix
@@ -1,57 +1,58 @@
 { pkgs, ... }:
 let
   dotfiles-dir = ../../../dotfiles/firefox;
-
   userContent-path = "${dotfiles-dir}/userContent";
-  userContent-files =
-    if builtins.pathExists userContent-path then
-      builtins.attrNames (builtins.readDir userContent-path)
-    else
-      [ ];
+  userContent-files = if builtins.pathExists userContent-path then
+    builtins.attrNames (builtins.readDir userContent-path)
+  else
+    [ ];
 
-  process-content = map (
-    filename:
+  valid-files = builtins.filter (f: f != ".keep") userContent-files;
+
+  stylusStyles = builtins.listToAttrs (map (filename:
     let
-      file-path = "${userContent-path}/${filename}";
-      is-less = pkgs.lib.hasSuffix ".less" filename;
       domain-name = pkgs.lib.removeSuffix ".less" (pkgs.lib.removeSuffix ".css" filename);
-    in
-    if is-less then
-      ''
-        @-moz-document domain("${domain-name}") {
-          ${builtins.readFile (
-            pkgs.runCommand "${domain-name}.css" { nativeBuildInputs = [ pkgs.nodePackages.less ]; } ''
-              lessc ${file-path} $out
-            ''
-          )}
-        }
-      ''
-    else if filename == ".keep" then
-      ""
-    else
-      ''
-        ${builtins.readFile file-path}
-      ''
-  ) userContent-files;
+      is-less = pkgs.lib.hasSuffix ".less" filename;
+      file-path = "${userContent-path}/${filename}";
 
-  final-user-content = pkgs.lib.concatStringsSep "\n" process-content;
+      compiled-css = if is-less then
+        builtins.readFile (pkgs.runCommand "${domain-name}.css" { nativeBuildInputs = [ pkgs.nodePackages.less ]; } ''
+          lessc ${file-path} $out
+        '')
+      else
+        builtins.readFile file-path;
+
+      userstyle-text = ''
+        /* ==UserStyle==
+        @name         Nix-${domain-name}
+        @namespace    givik-nix
+        @version      1.0.0
+        ==/UserStyle== */
+        @-moz-document domain("${domain-name}") {
+        ${compiled-css}
+        }
+      '';
+    in
+    {
+      name = ".config/stylus/userstyles/${domain-name}.user.css";
+      value = { text = userstyle-text; };
+    }
+  ) valid-files);
 
 in
 {
+  home.file = stylusStyles;
+
   programs.firefox = {
     enable = true;
     profiles.default = {
       name = "default";
       isDefault = true;
-
       settings = {
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-        "layers.acceleration.force-enabled" = true;
+       "toolkit.legacyUserProfileCustomizations.stylesheets" = false;
       };
-
+      userContent = "";
       userChrome = builtins.readFile "${dotfiles-dir}/userChrome/userChrome.css";
-
-      userContent = final-user-content;
     };
   };
 }
