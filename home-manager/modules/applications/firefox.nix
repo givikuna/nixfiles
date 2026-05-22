4634ca67ee225aded5056a595214605285e12bb5
Givikuna <thommaster10@gmail.com>
2026-05-22 19:51:42 -0400

    fuck firefooxooox

--- home-manager/modules/applications/firefox.nix
+++ home-manager/modules/applications/firefox.nix
@@ -1,11 +1,13 @@
 { pkgs, ... }:
 let
   dotfiles-dir = ../../../dotfiles/firefox;
+
   userContent-path = "${dotfiles-dir}/userContent";
-  userContent-files = if builtins.pathExists userContent-path then
-    builtins.attrNames (builtins.readDir userContent-path)
-  else
-    [ ];
+  userContent-files =
+    if builtins.pathExists userContent-path then
+      builtins.attrNames (builtins.readDir userContent-path)
+    else
+      [ ];
 
   valid-files = builtins.filter (f: f != ".keep") userContent-files;
 
@@ -22,19 +24,18 @@ let
       else
         builtins.readFile file-path;
 
-      userstyle-text = ''
+     userstyle-text = ''
         /* ==UserStyle==
         @name         Nix-${domain-name}
         @namespace    givik-nix
         @version      1.0.0
         ==/UserStyle== */
-        @-moz-document domain("${domain-name}") {
+
         ${compiled-css}
-        }
       '';
     in
     {
-      name = ".config/stylus/userstyles/${domain-name}.user.css";
+      name = ".config/stylus-styles/${filename}";
       value = { text = userstyle-text; };
     }
   ) valid-files);
@@ -48,11 +49,15 @@ in
     profiles.default = {
       name = "default";
       isDefault = true;
+
       settings = {
-       "toolkit.legacyUserProfileCustomizations.stylesheets" = false;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "layers.acceleration.force-enabled" = true;
       };
-      userContent = "";
+
       userChrome = builtins.readFile "${dotfiles-dir}/userChrome/userChrome.css";
+
+      userContent = "";
     };
   };
 }
