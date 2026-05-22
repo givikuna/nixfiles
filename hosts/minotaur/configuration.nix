5cf8084beffedce0e6b60a4dd7e3bde8e60951cf
Givikuna <thommaster10@gmail.com>
2026-05-22 15:45:32 -0400

    configruation commons nix

--- hosts/minotaur/configuration.nix
+++ hosts/minotaur/configuration.nix
@@ -5,130 +5,15 @@
     ../modules/boxes.nix
   ];
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
   networking.hostName = "minotaur";
-  networking.networkmanager.enable = true;
 
-  security.polkit.enable = true;
-
-  programs.hyprland.enable = true;
-  programs.fish.enable = true;
-  programs.zsh.enable = true;
-  environment.shells = with pkgs; [
-    fish
-    zsh
-    bash
-    nushell
+  environment.systemPackages = with pkgs; [
+    davinci-resolve
   ];
-
-  users.users.givik.shell = pkgs.fish;
-  security.pam.services.hyprlock = { };
-
-  # gives swayosd rights
-  services.udev.packages = [ pkgs.swayosd ];
-
-  # might have to move this up above hyprland and polkit, we'll see
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --asterisks --cmd /run/current-system/sw/bin/Hyprland";
-        user = "greeter";
-      };
-    };
-  };
-
-  systemd.services.greetd.serviceConfig = {
-    Type = "idle";
-    StandardInput = "tty";
-    StandardOutput = "tty";
-    StandardError = "journal";
-    TTYReset = true;
-    TTYVHangup = true;
-    TTYVTDisallocate = true;
-  };
-  # end of file block for the greeter
-
-  # might switch this away, mostly for video game purposes
-  boot.kernelPackages = pkgs.linuxPackages_zen;
-
-  # deprecated now
-  # systemd.enableUnifiedCgroupHierarchy = true;
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.roboto-mono
-    noto-fonts-color-emoji
-  ];
-
-  nixpkgs.config.allowUnfree = true;
-
-  users.users.givik = {
-    isNormalUser = true;
-    # shell = pkgs.fish; # declared in shell.nix now
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-      "video"
-      "input"
-    ];
-  };
-
-  security.rtkit.enable = true;
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-
-  services.tlp.enable = true;
-
-  # might move flatpak stuff to its own module=
-  services.flatpak = {
-    enable = true;
-    uninstallUnmanaged = false;
-
-    packages = [
-      "com.heroicgameslauncher.hgl"
-      "net.lutris.Lutris"
-    ];
-  };
-
-  # gaming optimization stuff:
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  programs.gamemode.enable = true;
-  programs.gamescope.enable = true;
-
-  # steam is being added via configuration.nix now instead of apps.nix
-  # this might not be the best idea, might switch if disliked
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
-  };
-  # might wanna add `nvidia-offload gamemoderun mangohud %command%` as the game's launch options on steam
 
   # nvidia driver stuff
   # if copying my system I would change the stuff here
   # this will not work on everyone's computers
-  # I'll keep these commented out for now until later
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = true;
@@ -151,68 +36,4 @@
       nvidiaBusId = "PCI:1:0:0";
     };
   };
-
-  # for screen-sharing (stops from getting a black screen)
-  xdg.portal = {
-    enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-hyprland
-    ];
-    config.common.default = "hyprland";
-  };
-
-  # force chromium based apps to use wayland
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-  };
-
-  # clean up system weekly
-  nix.optimise.automatic = true;
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
-
-  # to clean out solid-state blocks
-  services.fstrim.enable = true;
-
-  # for managing heat
-  services.thermald.enable = true;
-
-  # for proper usb reading
-  services.udisks2.enable = true;
-  services.gvfs.enable = true;
-
-  # keyring to avoid re-logins
-  services.gnome.gnome-keyring.enable = true;
-
-  hardware.xpadneo.enable = true;
-
-  # printing
-  services.printing.enable = true;
-
-  # this should intercept generic calls to binaries and re-connect it to nix store
-  # should help give IDEs better integration
-  programs.nix-ld = {
-    enable = true;
-    libraries = with pkgs; [
-      stdenv.cc.cc
-      zlib
-      glibc
-    ];
-  };
-
-  environment.pathsToLink = [ "/share/hypr" ];
-
-  #
-
-  time.timeZone = "America/New_York";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  console.keyMap = "us";
-
-  system.stateVersion = "23.11";
 }
