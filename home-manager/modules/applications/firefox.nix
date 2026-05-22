3956cbd026de441245b888978bef11689d3c542c
Givikuna <thommaster10@gmail.com>
2026-05-22 13:33:25 -0400

    version 5.6

--- home-manager/modules/applications/firefox.nix
+++ home-manager/modules/applications/firefox.nix
@@ -1,60 +1,57 @@
 { pkgs, ... }:
+let
+  dotfiles-dir = ../../../dotfiles/firefox;
+
+  userContent-path = "${dotfiles-dir}/userContent";
+  userContent-files =
+    if builtins.pathExists userContent-path then
+      builtins.attrNames (builtins.readDir userContent-path)
+    else
+      [ ];
+
+  process-content = map (
+    filename:
+    let
+      file-path = "${userContent-path}/${filename}";
+      is-less = pkgs.lib.hasSuffix ".less" filename;
+      domain-name = pkgs.lib.removeSuffix ".less" (pkgs.lib.removeSuffix ".css" filename);
+    in
+    if is-less then
+      ''
+        @-moz-document domain("${domain-name}") {
+          ${builtins.readFile (
+            pkgs.runCommand "${domain-name}.css" { nativeBuildInputs = [ pkgs.nodePackages.less ]; } ''
+              lessc ${file-path} $out
+            ''
+          )}
+        }
+      ''
+    else if filename == ".keep" then
+      ""
+    else
+      ''
+        ${builtins.readFile file-path}
+      ''
+  ) userContent-files;
+
+  final-user-content = pkgs.lib.concatStringsSep "\n" process-content;
+
+in
 {
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox;
+    profiles.default = {
+      name = "default";
+      isDefault = true;
+
+      settings = {
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "layers.acceleration.force-enabled" = true;
+      };
+
+      userChrome = builtins.readFile "${dotfiles-dir}/userChrome/userChrome.css";
+
+      userContent = final-user-content;
+    };
   };
 }
-
-#{ pkgs, ... }:
-#let
-#  dotfiles-dir = ../../../../firefox;
-#
-#  userContent-path = "${dotfiles-dir}/userContent";
-#  userContent-files = builtins.attrNames (builtins.readDir userContent-path);
-#
-#  process-content = map (
-#    filename:
-#    let
-#      file-path = "${userContent-path}/${filename}";
-#      is-less = pkgs.lib.hasSuffix ".less" filename;
-#      domain-name = pkgs.lib.removeSuffix ".less" (pkgs.lib.removeSuffix ".css" filename);
-#    in
-#    if is-less then
-#      ''
-#        @-moz-document domain("${domain-name}") {
-#          ${builtins.readFile (
-#            pkgs.runCommand "${domain-name}.css" { nativeBuildInputs = [ pkgs.nodePackages.less ]; } ''
-#              lessc ${file-path} $out
-#            ''
-#          )}
-#        }
-#      ''
-#    else
-#      ''
-#        ${builtins.readFile file-path}
-#      ''
-#  ) userContent-files;
-#
-#  final-user-content = pkgs.lib.concatStringsSep "\n" process-content;
-#
-#in
-#{
-#  programs.firefox = {
-#    enable = true;
-#    profiles.default = {
-#      name = "default";
-#      isDefault = true;
-#
-#      settings = {
-#        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-#        "layers.acceleration.force-enabled" = true;
-#      };
-#
-#      userChrome = builtins.readFile "${dotfiles-dir}/userChrome/userChrome.css";
-#
-#      userContent = final-user-content;
-#    };
-#  };
-#}
-#
