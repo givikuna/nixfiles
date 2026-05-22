8fb119ef9a418218f016bb07edfe9158bd472edf
Givikuna <thommaster10@gmail.com>
2026-05-22 19:57:26 -0400

    fuck stylus

--- home-manager/modules/applications/firefox.nix
+++ home-manager/modules/applications/firefox.nix
@@ -1,49 +1,8 @@
 { pkgs, ... }:
 let
   dotfiles-dir = ../../../dotfiles/firefox;
-
-  userContent-path = "${dotfiles-dir}/userContent";
-  userContent-files =
-    if builtins.pathExists userContent-path then
-      builtins.attrNames (builtins.readDir userContent-path)
-    else
-      [ ];
-
-  valid-files = builtins.filter (f: f != ".keep") userContent-files;
-
-  stylusStyles = builtins.listToAttrs (map (filename:
-    let
-      domain-name = pkgs.lib.removeSuffix ".less" (pkgs.lib.removeSuffix ".css" filename);
-      is-less = pkgs.lib.hasSuffix ".less" filename;
-      file-path = "${userContent-path}/${filename}";
-
-      compiled-css = if is-less then
-        builtins.readFile (pkgs.runCommand "${domain-name}.css" { nativeBuildInputs = [ pkgs.nodePackages.less ]; } ''
-          lessc ${file-path} $out
-        '')
-      else
-        builtins.readFile file-path;
-
-     userstyle-text = ''
-        /* ==UserStyle==
-        @name         Nix-${domain-name}
-        @namespace    givik-nix
-        @version      1.0.0
-        ==/UserStyle== */
-
-        ${compiled-css}
-      '';
-    in
-    {
-      name = ".config/stylus-styles/${filename}";
-      value = { text = userstyle-text; };
-    }
-  ) valid-files);
-
 in
 {
-  home.file = stylusStyles;
-
   programs.firefox = {
     enable = true;
     profiles.default = {
