24d34d6d8f351906552804978239eb531597b54d
Givikuna <thommaster10@gmail.com>
2026-05-22 14:38:35 -0400

    fuck versions completely

--- scripts/rebuild-system.py
+++ scripts/rebuild-system.py
@@ -12,24 +12,11 @@ def rebuild():
 
     ret = os.system("sudo nixos-rebuild switch --flake /etc/nixos#nixos")
 
-    version_mgmt()
-
     if ret == 0:
         print("system rebuilt successfully!")
     else:
         print("rebuild failed.")
         sys.exit(1)
-
-
-
-def version_mgmt():
-    with open("/etc/nixos/version.txt", "r") as f:
-        m_v, s_v = [int(v) for v in f.readline().strip().split(".")]
-        s_v = s_v + 1 if s_v <= 9 else 0
-        m_v = m_v if s_v != 0 else m_v + 1
-        n_v = f"{m_v}.{s_v}"
-        with open("/etc/nixos/version.txt", "w") as m_f:
-            m_f.write(n_v)
 
 if __name__ == "__main__":
     rebuild()
