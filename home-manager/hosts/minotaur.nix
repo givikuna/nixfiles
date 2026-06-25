0da6e1da2adc699d54f7c0e11e9e1464c2735145
Givi <gvtsvariani@gmail.com>
2026-06-24 22:59:41 -0400

    two new gitboy projets

--- home-manager/hosts/minotaur.nix
+++ home-manager/hosts/minotaur.nix
@@ -5,62 +5,115 @@
 
     ../modules/desktop/hyprgruvbox/desktop.nix
 
-    ../packages/configurations/applications/chronicler/package.nix
-    ../packages/configurations/applications/dolphin/package.nix
-    ../packages/configurations/applications/firefox/package.nix
-    ../packages/configurations/applications/gonwatch/package.nix
+    ########## packages
+
+    # programming
     ../packages/configurations/applications/helix/package.nix
-    ../packages/configurations/applications/kitty/package.nix
     ../packages/configurations/applications/micro/package.nix
     ../packages/configurations/applications/neovim/package.nix
+
+    # personal
+    ../packages/configurations/applications/chronicler/package.nix
+
+    # browsers
+    ../packages/configurations/applications/firefox/package.nix
+
+    # file managers
+    ../packages/configurations/applications/dolphin/package.nix
+
+    # piracy
+    ../packages/configurations/applications/gonwatch/package.nix
+
+    # terminal emulators
+    ../packages/configurations/applications/kitty/package.nix
+
+    # communications
     ../packages/configurations/applications/nixcord/package.nix
-    ../packages/configurations/applications/prismlauncher/package.nix
     ../packages/configurations/applications/thunderbird/package.nix
+
+    # gaming
+    ../packages/configurations/applications/prismlauncher/package.nix
   ];
 
   home.packages = with pkgs; [
+    # programming
     vscode
     qtcreator
-    google-chrome
+
+    # personal
     obsidian
     planify
+    skrooge
+
+    # browsers
+    google-chrome
+
+    # capturing
     gimp
     imagemagick
     switcheroo
-    haruna
-    mpv
-    imv
-    protonup-qt
-    mangohud
-    easyeffects
-    qbittorrent
+    obs-studio
+
+    # streaming services
+    spotify
+    spotify-player
+
+    # file managers
+    thunar
+
+    # privacy
+
+    # editing
+    davinci-resolve
+
+    # tools
     pdfarranger
     evince
     gnome-disk-utility
-    thunar
-    obs-studio
-    ffmpeg
     mission-center
-    spotify
-    spotify-player
-    davinci-resolve
+
+    # piracy
     ani-cli
-    libreoffice-fresh
-    bottles
+
+    # torrents
+    qbittorrent
+
+    # gaming
+    protonup-qt
+    mangohud
     itch
-    skrooge
+
+    # basics
+    haruna
+    mpv
+    imv
+    ffmpeg
+    libreoffice-fresh
+
+    # system
+    easyeffects
+
+    # virtualization
+    bottles
   ];
 
   services.flatpak.packages = [
+    # gaming
     "com.heroicgameslauncher.hgl"
     "net.lutris.Lutris"
     "ro.go.hmlendea.SokoGrump"
     "com.endlessnetwork.aqueducts"
     "sh.ppy.osu"
     "com.mojang.Minecraft"
-    "com.discordapp.Discord"
     "org.vinegarhq.Sober"
+
+    # communications
+    "com.discordapp.Discord"
+
+    # system
     "com.github.tchx84.Flatseal"
+
+    # torrents
     "de.haeckerfelix.Fragments"
   ];
 }
--- home-manager/hosts/nomad.nix
+++ home-manager/hosts/nomad.nix
@@ -1,8 +1,65 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ../common.nix
 
     ../modules/desktop/hyprgruvbox/desktop.nix
+
+    ../packages/configurations/applications/chronicler/package.nix
+    ../packages/configurations/applications/dolphin/package.nix
+    ../packages/configurations/applications/firefox/package.nix
+    ../packages/configurations/applications/gonwatch/package.nix
+    ../packages/configurations/applications/helix/package.nix
+    ../packages/configurations/applications/kitty/package.nix
+    ../packages/configurations/applications/micro/package.nix
+    ../packages/configurations/applications/neovim/package.nix
+    ../packages/configurations/applications/nixcord/package.nix
+    ../packages/configurations/applications/prismlauncher/package.nix
+    ../packages/configurations/applications/thunderbird/package.nix
+  ];
+
+  home.packages = with pkgs; [
+    vscode
+    qtcreator
+    obsidian
+    planify
+    gimp
+    imagemagick
+    switcheroo
+    haruna
+    mpv
+    imv
+    protonup-qt
+    mangohud
+    easyeffects
+    qbittorrent
+    pdfarranger
+    evince
+    gnome-disk-utility
+    thunar
+    obs-studio
+    ffmpeg
+    mission-center
+    spotify
+    spotify-player
+    davinci-resolve
+    ani-cli
+    libreoffice-fresh
+    bottles
+    itch
+    skrooge
+  ];
+
+  services.flatpak.packages = [
+    "com.heroicgameslauncher.hgl"
+    "net.lutris.Lutris"
+    "ro.go.hmlendea.SokoGrump"
+    "com.endlessnetwork.aqueducts"
+    "sh.ppy.osu"
+    "com.mojang.Minecraft"
+    "com.discordapp.Discord"
+    "org.vinegarhq.Sober"
+    "com.github.tchx84.Flatseal"
+    "de.haeckerfelix.Fragments"
   ];
 }
