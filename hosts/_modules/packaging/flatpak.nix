f7ef54840704ca435ee588a78ed372fb9633f94c
Givi <gvtsvariani@gmail.com>
2026-06-27 22:05:44 -0400

    flatpak

--- hosts/_modules/packaging/flatpak.nix
+++ hosts/_modules/packaging/flatpak.nix
@@ -3,5 +3,16 @@
   services.flatpak = {
     enable = true;
     uninstallUnmanaged = false;
+
+    remotes = [
+      {
+        name = "flathub";
+        location = "https://flathub.org/repo/flathub.flatpakrepo";
+      }
+      {
+        name = "flathub-beta";
+        location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+      }
+    ];
   };
 }
